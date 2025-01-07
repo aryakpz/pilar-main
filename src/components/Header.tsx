0db8119b410ad5a09759bb099196f7c3730c8d41
Code Malayalam <aryamolsanthosh962@gmail.com>
2025-01-07 17:24:08 +0530

    feat: Add the header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+
+export const Header = () => {
+    return (
+      <div className="border-b w-full z-50">
+        <nav className="border-gray-200 bg-white px-2 py-2.5 sm:px-4">
+          <div className="flex flex-wrap items-center justify-between mx-auto">
+            <a className="flex items-center">
+              <img src="/assets/images/pilar-logo.svg" alt="pilar-left-logo" />
+            </a>
+            <div className="w-fit">
+              <button className="flex items-center">
+                <div className="flex items-center space-x-4">
+                  <div className="relative">
+                    <img
+                      className="rounded-3xl w-10 h-10"
+                      src="/assets/images/pilar-nav-image.png"
+                      alt="pilar-right-log"
+                    />
+                  </div>
+                </div>
+              </button>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+  
--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "../components/Button";
 import { useNavigate } from "react-router-dom";
 import { ButtonType, ButtonVariant } from "../types/enum";
+import { Header } from "../components/Header";
 
 export const AdminPage: React.FC = () => {
     const nav = useNavigate()
@@ -12,22 +13,25 @@ export const AdminPage: React.FC = () => {
 
     return (
         < div >
-            <Button
-                label=" +  New portfolio"
-                type={ButtonType.BUTTON}
-                onClick={handleClick}
-                style=""
-                variant={ButtonVariant.DARK}
-            />
-            <div className="p-5">
+            <Header />
+            <div>
+                <Button
+                    label=" +  New portfolio"
+                    type={ButtonType.BUTTON}
+                    onClick={handleClick}
+                    style=""
+                    variant={ButtonVariant.DARK}
+                />
+                <div className="p-5">
+                </div>
+                <Button
+                    label="View More"
+                    type={ButtonType.BUTTON}
+                    onClick={handleClick}
+                    style=""
+                    variant={ButtonVariant.LIGHT}
+                />
             </div>
-            <Button
-                label="View More"
-                type={ButtonType.BUTTON}
-                onClick={handleClick}
-                style=""
-                variant={ButtonVariant.LIGHT}
-            />
         </div >
     )
 }
