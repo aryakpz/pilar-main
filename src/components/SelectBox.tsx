c96971917a72f11e93a318694bce991acc25888d
Code Malayalam <aryamolsanthosh962@gmail.com>
2025-01-08 14:14:40 +0530

    fix:return selected country into parent component

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -4,16 +4,13 @@ import { SelectboxProps } from "../types/type"
 
 
 export const SelectBox: React.FC<SelectboxProps> = ({ onSelectChange }) => {
-    const { data } = useJsonFetch()
-    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        onSelectChange(e.target.value)
-    }
+    const { data } = useJsonFetch();
 
     return (
         <div className="flex flex-col w-40 p-5">
             <span className="font-medium text-sm text-gray-900 pb-4">Country</span>
             <select className="text-sm p-2 rounded-md block bg-white border w-max appearance-none bg-down-arrow bg-no-repeat bg-arrow-position"
-                onChange={handleChange}>
+                onChange={(e)=>onSelectChange(e.target.value)}>
                 <option value=""></option>
                 {data?.countries.map((item: string) => (
                     <option key={item} value={item}>{item}</option>
