04b692d0880614b1c27ae8af4f0db4e11cdabda2
Code Malayalam <aryamolsanthosh962@gmail.com>
2025-01-09 13:30:47 +0530

    feat: add selectbox to sort

---
+++ src/components/SortSelectbox.tsx
@@ -0,0 +1,19 @@
+import { SortSelect } from "../types"
+
+type SelectProp = {
+    onChangeSelector: (value: SortSelect) => void
+}
+
+export const SortSelector: React.FC<SelectProp> = ({ onChangeSelector }) => {
+ 
+    return (
+        <div className="w-16 flex">
+            <select className="block w-full appearance-none bg-down-arrow bg-no-repeat bg-arrow-position text-sm p-2 rounded-lg border bg-white"
+                onChange={(e)=>onChangeSelector(e.target.value as SortSelect)} >
+                <option value={SortSelect.ASC}>{SortSelect.ASC}</option>
+                <option value={SortSelect.DES}>{SortSelect.DES}</option>
+            </select>
+        </div>
+    )
+}    
+
