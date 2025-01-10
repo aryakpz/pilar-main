2c11746746d3925908cebd9ee1e8d8bb18895ee3
Code Malayalam <aryamolsanthosh962@gmail.com>
2025-01-10 09:22:44 +0530

    feat: add portfolio cards

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -11,10 +11,10 @@ export const MainBody = () => {
                     <div className="flex flex-col p-4 pt-0">
                         <div className="flex items-center">
                             <span className="flex-1 font-semibold text-lg ">{item.title}</span>
-                            <button className="p-2 -rotate-90">
+                            <button className="p-2">
                                 <div className="w-fit">
-                                    <button className="text-gray-500 bg-white border border-gray-300 hover:bg-gray-400 p-0.5 text-center font-medium rounded-lg group flex h-min items-center justify-center">
-                                        <span className="flex items-center rounded-md text-xs px-1 pb-2 justify-center align-middle"> ...
+                                    <button className="text-gray-500 bg-white border border-gray-300 hover:bg-gray-400 p-0.5 pr-[0px] text-center  font-medium rounded-lg group flex h-min items-center justify-center">
+                                        <span className="flex items-center rounded-md text-xs justify-center align-middle rotate-90 pl-3 pr-2 pb-1 pt-1  mr-[-4px] tracking-widest "> ...
                                         </span>
                                     </button>
                                 </div>
--- src/pages/Portfolios.tsx
+++ src/pages/Portfolios.tsx
@@ -8,10 +8,9 @@ export const Portfolio: React.FC = () => {
             <main className="w-full h-full overflow-y-auto pt-10 bg-gray-100">
                 <div className="px-12 pb-5 sm:px-24 md:px-28 lg:px-36">
                     <Greetings />
-                    <MainBody/>
+                    <MainBody />
                 </div>
             </main>
         </div>
     )
 }
-
