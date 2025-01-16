351dc52373429332d338ca57ede98ba9a51a060c
Code Malayalam <aryamolsanthosh962@gmail.com>
2025-01-16 14:59:07 +0530

    feat: adding portfolio complete form

--- src/components/CurrencySelectbox.tsx
+++ src/components/CurrencySelectbox.tsx
@@ -13,13 +13,13 @@ const CurrencyDataType = [
 
 export const CurrencySelectbox: React.FC<CurrecyProps> = ({ handleSelectCurrency }) => {
     return (
-        <div className="flex-1 md:basic-1/2 pr-6">
+        <div className="flex-1 md:basis-1/2 pr-6">
             <div className="flex flex-col gap-2 w-full">
                 <label className="font-medium text-sm text-gray-900">Choose currency</label>
                 <div className="flex">
                     <div className="relative w-full">
                         <select
-                            className="block w-full border rounded-lg p-2.5 text-sm appearance-none bg-white bg-down-arrow bg-no-repeat bg-size pr-5 bg-arrow-position"
+                            className="block w-full border rounded-lg p-2.5 text-sm appearance-none bg-slate-50 bg-down-arrow bg-no-repeat bg-size pr-5 bg-arrow-position"
                             onChange={(e) => handleSelectCurrency(e.target.value as CurrencyType)}
                         >
                             <option value="" hidden></option>
--- src/pages/AddPortfolio.tsx
+++ src/pages/AddPortfolio.tsx
@@ -3,7 +3,7 @@ import { PortfolioFormNav } from "../components"
 
 export const AddPortfolis = () => {
     return (
-        <div className="bg-gray-100 h-screen">
+        <div className="bg-gray-100 h-full">
             <PortfolioFormNav />
             <AddPortfolioFormBody />
         </div>
